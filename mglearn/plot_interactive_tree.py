2da7230aef4adc47a217a66b09dad8693a664955
Adam Baso <abaso@wikimedia.org>
2016-10-13 22:32:57 -0500

    Update plot_interactive_tree.py

--- mglearn/plot_interactive_tree.py
+++ mglearn/plot_interactive_tree.py
@@ -5,7 +5,7 @@ from sklearn.tree import DecisionTreeClassifier
 
 from sklearn.externals.six import StringIO  # doctest: +SKIP
 from sklearn.tree import export_graphviz
-from scipy.misc import imread
+from scipy.ndimage import imread
 from scipy import ndimage
 from sklearn.datasets import make_moons
 

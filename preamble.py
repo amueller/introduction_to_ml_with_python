78cb8c0b13c9619964082eee2491fac6019fbec5
Andreas Mueller <t3kcit@gmail.com>
2017-02-24 15:01:58 -0500

    fix preamble for matplotlib v2

--- preamble.py
+++ preamble.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import mglearn
 from cycler import cycler
 
-#set_matplotlib_formats('png', 'svg')
 set_matplotlib_formats('pdf', 'png')
 plt.rcParams['savefig.dpi'] = 300
 plt.rcParams['image.cmap'] = "viridis"
@@ -13,15 +12,13 @@ plt.rcParams['image.interpolation'] = "none"
 plt.rcParams['savefig.bbox'] = "tight"
 plt.rcParams['lines.linewidth'] = 2
 plt.rcParams['legend.numpoints'] = 1
-plt.rc('axes', prop_cycle=(cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
-                           cycler('linestyle', ['-', '--', ':',
-                                                '-.', '--'])
-                           )
-       )
+plt.rc('axes', prop_cycle=(
+    cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
+    cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
 
 np.set_printoptions(precision=3, suppress=True)
 
 pd.set_option("display.max_columns", 8)
 pd.set_option('precision', 2)
 
-np, mglearn
+__all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
